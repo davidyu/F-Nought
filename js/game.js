40f219152888c7c5a80f036a789d74047827d20b
Desktop <lewen.yu@gmail.com>
2013-02-22 20:50:51 -0800

    fixed integration issue by explicitly calling init

--- js/game.js
+++ js/game.js
@@ -109,7 +109,7 @@ var Game = {
 var Settings = {
 
     fps           : 60,                      // how many 'update' frames per second
-    step          : 1/this.fps,              // how long is each frame (in seconds)
+    step          : 1/60,                    // how long is each frame (in seconds)
     width         : 1024,                    // logical canvas width
     height        : 768,                     // logical canvas height
     segments      : [],                      // array of road segments
@@ -139,6 +139,44 @@ var Settings = {
     offRoadDecel  : -this.maxSpeed/2,             // off road deceleration is somewhere in between
     offRoadLimit  :  this.maxSpeed/4,             // limit when off road deceleration no longer applies (e.g. you can always go at least this speed even when off road)
 
+    //must call init! Some values in Settings null
+    init: function() {
+        this.fps           = 60;                      // how many 'update' frames per second
+        this.step          = 1/this.fps;                   // how long is each frame (in seconds)
+        this.width         = 1024;                    // logical canvas width
+        this.height        = 768;                     // logical canvas height
+        this.segments      = [];                      // array of road segments
+        this.canvas        = Dom.get('canvas');       // our canvas...
+        this.ctx           = this.canvas.getContext('2d'); // ...and its drawing context
+        this.background    = null;                    // our background image (loaded below)
+        this.sprites       = null;                    // our spritesheet (loaded below)
+        this.resolution    = null;                    // scaling factor to provide resolution independence (computed)
+        this.roadWidth     = 2000;                    // actually half the roads width, easier math if the road spans from -roadWidth to +roadWidth
+        this.segmentLength = 200;                     // length of a single segment
+        this.rumbleLength  = 3;                       // number of segments per red/white rumble strip
+        this.trackLength   = null;                    // z length of entire track (computed)
+        this.lanes         = 3;                       // number of lanes
+        this.fieldOfView   = 100;                     // angle (degrees) for field of view
+        this.cameraHeight  = 1000;                    // z height of camera
+        this.cameraDepth   = null;                    // z distance camera is from screen (computed)
+        this.drawDistance  = 300;                     // number of segments to draw
+        this.playerX       = 0;                       // player x offset from center of road (-1 to 1 to stay independent of roadWidth)
+        this.playerZ       = null;                    // player relative z distance from camera (computed)
+        this.fogDensity    = 5;                       // exponential fog density
+        this.position      = 0;                       // current camera Z position (add playerZ to get player's absolute Z position)
+        this.speed         = 0;                       // current speed
+        this.maxSpeed      = this.segmentLength/this.step;      // top speed (ensure we can't move more than 1 segment in a single frame to make collision detection easier)
+        this.accel         =  this.maxSpeed/5;             // acceleration rate - tuned until it 'felt' right
+        this.breaking      = -this.maxSpeed;               // deceleration rate when braking
+        this.decel         = -this.maxSpeed/5;             // 'natural' deceleration rate when neither accelerating, nor braking
+        this.offRoadDecel  = -this.maxSpeed/2;             // off road deceleration is somewhere in between
+        this.offRoadLimit  =  this.maxSpeed/4;             // limit when off road deceleration no longer applies (e.g. you can always go at least this speed even when off road)
+
+        this.keyLeft       = false;
+        this.keyRight      = false;
+        this.keyFaster     = false;
+        this.keySlower     = false;
+    },
 
     reset: function( options ) {
       options       = options || {};
